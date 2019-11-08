725739d0b662a754be4b4dead6bb5ecd84b9ab02
Kizito Njoku <nkizito57@gmail.com>
2019-11-08 03:37:59 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -8,4 +8,5 @@ def my_find(collection)
   end 
 end
 collection = (1..100).to_a
-my_find(collection) {|i| i % 3 == 0 and i % 5 == 0 }
+binding.pry 
+my_find(collection) {|i| i % 3 == 0 and i % 5 == 0}
